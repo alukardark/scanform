385501001a04bf1536f186116d76361b27ea3ce8
Андрей Рябченко <ryabchenko.a@delcomp.ru>
2021-06-05 04:30:28 +0700

    'x'

--- js/common.js
+++ js/common.js
@@ -1,83 +1,3 @@
-const colors = ['#9ea2ec', '#9ea2ec', '#9ea2ec', '#9ea2ec'];
-const backgroundColor = '#EFEEF3';
-const widthCanvas = Math.min(document.body.clientWidth, 2000);
-const heightCanvas = Math.min(window.innerHeight, 435);
-const totalFrames = 2000;
-var frameCount = 0;
-
-
-function easeInQuint(x) {
-    return x * x * x;
-}
-
-function easeOutQuart(x) {
-    return 1 - pow(1 - x, 4);
-}
-
-// function setup() {
-//     var canvas = createCanvas(widthCanvas, heightCanvas);
-//
-//     canvas.parent("id-canvas");
-//
-//     noiseSeed(50);
-//     var bg = color(backgroundColor);
-//     background(bg);
-// }
-//
-// function draw() {
-//     frameCount += 4;
-//
-//     var frameDelta = 2 * Math.PI * (frameCount % totalFrames) / totalFrames;
-//
-//     var bg = color(backgroundColor);
-//     background(bg);
-//
-//     var xStart = 0;
-//     var yStart = 0;
-//     var w = xStart + widthCanvas;
-//     var h = yStart + heightCanvas;
-//
-//     var yDistance = 28;
-//     var xDistance = 32;
-//
-//     for (var y = yStart; y < h; y += yDistance) {
-//         for (var x = xStart; x < w; x += xDistance) {
-//             var _x = x;
-//
-//             var noiseOffset = frameDelta;
-//             var offsetY = noise(x * 0.01, -noiseOffset + y * 0.01) * 2.5;
-//             offsetY = Math.pow(offsetY, 6);
-//
-//             var _y = y - Math.abs(Math.sin(frameDelta) * offsetY);
-//
-//             var size = Math.abs(Math.sin(frameDelta) * (offsetY / 20));
-//             size = Math.max(size, 1.5);
-//             //
-//             // size = 1.5;
-//
-//             var pct = Math.abs(Math.sin(frameDelta) * (offsetY / 50));
-//
-//
-//             var colorA = color(colors[0]);
-//             var colorB = color(colors[3]);
-//             var colorC = color(colors[2]);
-//             pct = easeInQuint(pct);
-//             var _color = lerpColor(colorA, colorB, pct);
-//
-//             var restPCT = Math.abs(Math.sin(frameDelta));
-//             restPCT = easeOutQuart(restPCT);
-//             _color = lerpColor(colorC, _color, restPCT);
-//
-//             strokeWeight(1);
-//             stroke(_color);
-//             fill(_color);
-//
-//             circle(_x, _y, size);
-//         }
-//     }
-// }
-
-
 jQuery(document).ready(function ($) {
 
     $("input[type='tel']").inputmask({"mask": "+7(999) 999-9999"});
@@ -109,12 +29,38 @@ jQuery(document).ready(function ($) {
         },
     });
 
+    new Swiper('.main-clients .swiper-container', {
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        speed: 600,
+        // autoHeight: true,
+        // autoplay: {
+        //     delay: 3000
+        // },
+        navigation: {
+            nextEl: '.main-clients .main-clients__next',
+            prevEl: '.main-clients .main-clients__prev',
+        },
+
+        breakpoints: {
+            989: {
+                slidesPerView: 2,
+            },
+            767: {
+                slidesPerView: 2,
+            },
+            575: {
+                slidesPerView: 1,
+            },
+        },
+    });
+
     document.querySelectorAll('.scrollbar').forEach(el => {
         new SimpleBar(el, {
             autoHide: false
         });
     });
-
 
     var width = $(window).width();
 
@@ -139,6 +85,16 @@ jQuery(document).ready(function ($) {
                 $('.header').removeClass('active');
             }
         }
+    });
+
+
+    $('input[type=radio]').parent('span').addClass('radio-wrap');
+    $('input[value=zoom], input[value=Zoom]').parent('span').addClass('zoom-ico');
+    $('input[value=skype], input[value=Skype]').parent('span').addClass('skype-ico');
+
+    $('input[type=radio]').click(function(){
+        $(this).parents('form').find('span').removeClass('active');
+        $(this).parent('span').addClass('active');
     });
 
 });
